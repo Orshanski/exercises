ddee5151af97d1c0bf4af14fb4e5cc87881ae72d
aorshanskiy <alexey.orshanski@gmail.com>
2020-02-17 13:34:23 +0300

    14 задача (часть 4-5) восьмой главы Гэддиса

--- Gaddis/G8.14.py
+++ Gaddis/G8.14.py
@@ -103,9 +103,47 @@ for year in range(1993, 2014):
     min_price_in_year[year] = prices_list[min_index]
     max_price_in_year[year] = prices_list[max_index]
 
-
 for year in range(1993, 2014):
     print(f'В {min_price_in_year[year][2]} минимальная цена топлива была ${min_price_in_year[year][3]:.2f}, а '
           f'максимальная - ${max_price_in_year[year][3]:.2f}')
 
-#TODO Список цен, упорядоченный по возрастанию
+# Формируем файл со списком цен, упорядоченный по возрастанию
+new_price_list = []
+
+while not len(prices_list) == 0:
+    min_price = prices_list[0][3]
+    min_index = 0
+    for index in range(len(prices_list)):
+        if min_price > prices_list[index][3]:
+            min_price = prices_list[index][3]
+            min_index = index
+    new_price_list.append(prices_list.pop(min_index))
+
+low_to_high = open('Data/low_to_high', 'w')
+high_to_low = open('Data/high_to_low', 'w')
+new_string = ''
+for element in new_price_list:
+    if len(str(element[0])) > 1:
+        day = str(element[0])
+    else:
+        day = '0' + str(element[0])
+    if len(str(element[1])) > 1:
+        month = str(element[1])
+    else:
+        month = '0' + str(element[1])
+    new_string = day + '-' + month + '-' + str(element[2]) + ':' + f'{element[3]:.3f}' + '\n'
+    low_to_high.writelines(new_string)
+
+new_string = ''
+for index in range(len(new_price_list)-1, -1, -1):
+    element = new_price_list[index]
+    if len(str(element[0])) > 1:
+        day = str(element[0])
+    else:
+        day = '0' + str(element[0])
+    if len(str(element[1])) > 1:
+        month = str(element[1])
+    else:
+        month = '0' + str(element[1])
+    new_string = day + '-' + month + '-' + str(element[2]) + ':' + f'{element[3]:.3f}' + '\n'
+    high_to_low.writelines(new_string)
