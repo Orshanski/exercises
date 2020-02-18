549c9e1280cc7825a58a68fe9fade1d39ce8768a
aorshanskiy <alexey.orshanski@gmail.com>
2020-02-18 12:29:26 +0300

    Небольшие правки в текстовые файлы и функцию очистки текста

--- my_function.py
+++ my_function.py
@@ -2,7 +2,7 @@ def clear_text(text):
     """Очищает слова в тексте от знаков препинания и дефисов"""
     for index in range(len(text)):
         text[index] = text[index].lower()
-        if text[index][-1] in ('.', '?', '!', '"', ','):
+        if text[index][-1] in ('.', '?', '!', '"', ',', ':'):
             ch = text[index][-1]
             text[index] = text[index].rstrip(ch)
         if text[index][0] == '"':
@@ -15,6 +15,7 @@ def clear_text(text):
             break
 
     return text
+
 
 def decline_the_word(word, count):
     if word == 'раз' and count in [2, 3, 4]:
