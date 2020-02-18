8bcb2639a0631d30ee53f44d090f7d297273bb78
aorshanskiy <alexey.orshanski@gmail.com>
2020-02-18 10:58:18 +0300

    Третья задача девятой главы Гэддиса

--- Gaddis/Ch9/G93.py
+++ Gaddis/Ch9/G93.py
@@ -10,7 +10,7 @@ CODE = {'А': ')', 'а': '0', 'Б': '(', 'б': '9', 'В': '*', 'в': '8',
         'Х': 'O', 'х': 'o', 'Ц': 'N', 'ц': 'n', 'Ч': 'M', 'ч': 'm',
         'Ш': 'L', 'ш': 'l', 'Щ': 'K', 'щ': 'k', 'Ъ': 'я', 'ъ': 'Ы',
         'Э': 'А', 'э': 'а', 'Ю': 'з', 'ю': 'З', 'Я': 'о', 'я': 'о',
-        '!': 'J', '@': 'I',
+        '!': 'J', '@': 'I', 'ь': '|', '\n': '\n', ' ': ' ', 'ы': '/', '_': '_',
         '1': 'j', '2': 'i', '#': 'H', '3': 'h', '$': 'G', '4': 'g',
         '%': 'F', '5': 'f', '^': 'E', '6': 'e', '&': 'D', '7': 'd',
         '*': 'C', '8': 'c', '(': 'B', '9': 'b', ')': 'A', '0': 'a',
@@ -20,6 +20,27 @@ CODE = {'А': ')', 'а': '0', 'Б': '(', 'б': '9', 'В': '*', 'в': '8',
 
 infile = open('../Data/wiedzmin', 'r', encoding='utf-8')
 text = infile.read()
-text = text.split()
 
-print(wp.clear_text(text))
+text_encoded = ''
+for ch in text:
+    ch = CODE[ch]
+    text_encoded += ch
+
+file_encoded = open('../Data/wiedzmin_encoded', 'w')
+file_encoded.write(text_encoded)
+file_encoded.close()
+
+new_code = {}
+for key, value in CODE.items():
+    new_code[value] = key
+
+file_encoded = open('../Data/wiedzmin_encoded', 'r')
+text_encoded = file_encoded.read()
+
+text_decoded = ''
+for ch in text_encoded:
+    ch = new_code[ch]
+    text_decoded += ch
+
+print(text_decoded)
+
